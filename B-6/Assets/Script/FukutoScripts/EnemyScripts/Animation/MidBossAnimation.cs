e18e0dcc90f7da88bf26d45ca5122bcab7195a0e
pura55 <nekotonaito2@gmail.com>
2026-09-15 20:46:34 +0900

    大ボスのスキル（チャージ）を追加

--- B-6/Assets/Script/FukutoScripts/EnemyScripts/Animation/MidBossAnimation.cs
+++ B-6/Assets/Script/FukutoScripts/EnemyScripts/Animation/MidBossAnimation.cs
@@ -9,10 +9,19 @@ using UnityEngine.U2D;
 /// </summary>
 public class MidBossAnimation : IncludeMovementAnimation
 {
+    #region Config
+    [Header("CHARGE")]
+    [SerializeField] private bool isCharge = false; // チャージフラグ（スキルでチャージをするかどうか）
+    [SerializeField] private int stopSpriteIndex = 1; // 停止するスプライトの指数
+    [SerializeField] private float chargeTime = 3f; // 溜めの時間（目標）
+    #endregion
+
     #region State
     protected Sprite[] skillSprites;     // スキルスプライト
     protected int skillSpriteElements = 0; // スキルスプライトの要素数
     protected string skillSpriteName = "SKILL";   // スキルスプライト名
+
+    private float chargeCount = 0f; // 溜めのカウント
     #endregion
 
     void Start()
@@ -30,6 +39,10 @@ public class MidBossAnimation : IncludeMovementAnimation
         {
             case AnimationState.idle:
                 IdleAnimation();
+                if(isCharge)
+                {
+                    chargeCount = 0f;
+                }
                 break;
             case AnimationState.move:
                 MoveAnimation();
@@ -76,8 +89,52 @@ public class MidBossAnimation : IncludeMovementAnimation
     /// @brief 移動アニメーション
     protected void SkillAnimation()
     {
-        ManageEventFrame(skillSpriteElements);
+        if(!isCharge)
+        {
+            ManageEventFrame(skillSpriteElements);
+        }
+        else
+        {
+            ManageEventFrame(skillSpriteElements, stopSpriteIndex);
+        }
+
         spriteRenderer.sprite = skillSprites[spriteIndex];
+    }
+
+    /// @brief イベント時のフレーム管理を行う関数 
+    /// @param stopIndex 溜め攻撃を行うタイミングの指数
+    protected void ManageEventFrame(int elements, int stop)
+    {
+        if(chargeTime < chargeCount)
+        {
+            ManageEventFrame(elements);
+            
+        }
+        else
+        {
+            // 現在の時間がスプライトごとの時間よりも小さい場合
+            if (currentTime < timePerSprite)
+            {
+                currentTime += Time.deltaTime; // 時間を進める
+            }
+            else
+            {
+                // 指数 + 1 がより大きかったら指数を戻す
+                if (stop == spriteIndex)
+                {
+                    chargeCount += Time.deltaTime;
+                    return;
+                }
+                else
+                {
+                    // スプライト指数を進める
+                    spriteIndex++;
+                }
+
+                // フレームをリセット
+                currentTime = 0f;
+            }
+        }
     }
 
     /// @brief 移動スプライトを設定する関数
